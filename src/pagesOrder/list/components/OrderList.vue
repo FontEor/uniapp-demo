7eda12f994b65edbc3ac94b63897c3ee6bfa247d
FontEor <583139315@qq.com>
2024-10-18 14:42:02 +0800

    feat: 项目打包

--- src/pagesOrder/list/components/OrderList.vue
+++ src/pagesOrder/list/components/OrderList.vue
@@ -40,9 +40,11 @@ const onOrderPay = async (id: string) => {
     // 开发环境：模拟支付，修改订单状态为已支付
     await getPayMockAPI({ orderId: id })
   } else {
+    // #ifdef MP-WEIXIN
     // 生产环境：获取支付参数 + 发起微信支付
     const res = await getPayWxPayMiniPayAPI({ orderId: id })
     await wx.requestPayment(res.result)
+    // #endif
   }
   //支付成功
   uni.showToast({ title: '支付成功' })
